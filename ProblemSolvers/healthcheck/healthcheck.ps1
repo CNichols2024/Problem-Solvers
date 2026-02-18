3a3ca488333cca14470f191f2d89142c7a21d8c4
Chris Nichols <159453598+CNichols2024@users.noreply.github.com>
2026-02-18 12:02:47 -0500

    Add files via upload
    This branch/upload is meant to be testing if domain checks and repairs are reasonable for this script's objective.

--- ProblemSolvers/healthcheck/healthcheck.ps1
+++ ProblemSolvers/healthcheck/healthcheck.ps1
@@ -6,25 +6,30 @@ $basecpuSpeed = (Get-CimInstance -Class Win32_Processor).MaxClockSpeed / 1000
 $MaxClockSpeed = ((Get-CimInstance CIM_Processor).MaxClockSpeed) / 1000
 $ProcessorPerformance = (Get-Counter -Counter "\Processor Information(_Total)\% Processor Performance").CounterSamples.CookedValue
 $actualCPUSpeed = ($MaxClockSpeed * ($ProcessorPerformance / 100)) <#obtaining current cpu speed by multiplying the max clock speed by 100th the performance#>
-$trimmedActualCPUSpeed = $actualCPUSpeed.ToString().Substring(0, 4) <##>
+$trimmedActualCPUSpeed = $actualCPUSpeed.ToString().Substring(0, 4) <#trimming the speed to the first 2 digits after decimal#>
 
-<#testing string/variable results#>
-Write-Host "Max Clock Speed: $MaxClockSpeed GHz"
-Write-Host "CPU Speed: $trimmedActualCPUSpeed GHz"
+<#Getting Domain connection Status and name#>
+$domainCheck = (Get-CimInstance Win32_ComputerSystem).PartOfDomain
+$domainName = (Get-CimInstance Win32_ComputerSystem).Domain
+write-host = "$domainCheck"
+Write-Host = "$domainName"
+
+
 
 <#building html report#>
 $htmlContent = @"
 <!DOCTYPE html>
-<html>
+<html style="font-family: Arial, sans-serif;"> <!--setting font for the report-->
 <head>
     <title>PC Health Check</title>
 </head>
-<body>
+<body style="background-color: #3cb4e5;"> <!--setting background color for the report-->
     <h1> Username: $userDetails</h1>
-    <p>uptime: $uptime</p>
+    <p>Uptime: $uptime</p>
     <p>Base CPU Speed: $basecpuSpeed Ghz</p>
     <p>Current CPU Speed: $trimmedActualCPUSpeed Ghz</p>
+    <p>Domain Connection status: $domainName / $domainCheck</p>
 </body>
 </html>
 "@
-$htmlContent | Out-File -FilePath "C:\Users\$userName\healthcheck.html"
+$htmlContent | Out-File -FilePath "C:\Users\$userName\healthcheck.html" <#exporting html file to user's profile directory#>
