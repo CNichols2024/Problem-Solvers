644027e5bf3bd51d8c5f2a0d2d7a866b3023ea69
Chris Nichols <159453598+CNichols2024@users.noreply.github.com>
2026-02-19 16:58:13 -0500

    Add files via upload
    Completed and did light testing on domain checks. Added a few other features as well.
    Added and placed battery health information in a try/catch/finally to account for VM and desktops
    Currently, the exported data is:
    User Details
    Last Boot
    Base CPU speed vs current CPU speed
    Domain name, and verification of connection in true/false
    Battery health info including designed capacity, actual capacity and capacity percentage.

--- ProblemSolvers/healthcheck/healthcheck.ps1
+++ ProblemSolvers/healthcheck/healthcheck.ps1
@@ -1,5 +1,5 @@
 <#setting variables for html report#>
-$uptime = get-uptime -since
+$lastBoot = get-uptime -since
 $userDetails = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $username = $userDetails.Split('\')[-1] <#splitting username and removing the "\WNSM\" to use for output path#>
 $basecpuSpeed = (Get-CimInstance -Class Win32_Processor).MaxClockSpeed / 1000
@@ -7,15 +7,21 @@ $MaxClockSpeed = ((Get-CimInstance CIM_Processor).MaxClockSpeed) / 1000
 $ProcessorPerformance = (Get-Counter -Counter "\Processor Information(_Total)\% Processor Performance").CounterSamples.CookedValue
 $actualCPUSpeed = ($MaxClockSpeed * ($ProcessorPerformance / 100)) <#obtaining current cpu speed by multiplying the max clock speed by 100th the performance#>
 $trimmedActualCPUSpeed = $actualCPUSpeed.ToString().Substring(0, 4) <#trimming the speed to the first 2 digits after decimal#>
-
 <#Getting Domain connection Status and name#>
 $domainCheck = (Get-CimInstance Win32_ComputerSystem).PartOfDomain
 $domainName = (Get-CimInstance Win32_ComputerSystem).Domain
-write-host = "$domainCheck"
-Write-Host = "$domainName"
-
-
-
+<#Getting battery capacity details. Try/catch to account for desktops/VM#>
+try {
+    $designedCapacity = (Get-WmiObject -Class "BatteryStaticData" -Namespace "ROOT\WMI").DesignedCapacity
+    $actualCapacity = (Get-CimInstance -ClassName BatteryFullChargedCapacity -Namespace ROOT/WMI).FullChargedCapacity
+    $capacityPercentage = $actualCapacity / $designedCapacity * 100
+    $designedCapacity = [string]$designedCapacity + " mHw" #appending units to the capacity values for better readability in the report
+} catch {
+    
+} finally {
+    write-host("Battery information retrieval attempted. Check variables for results or errors.")
+}
+Write-Host("Designed Capacity: $designedCapacity") #outputting battery information to console for testing purposes
 <#building html report#>
 $htmlContent = @"
 <!DOCTYPE html>
@@ -25,10 +31,16 @@ $htmlContent = @"
 </head>
 <body style="background-color: #3cb4e5;"> <!--setting background color for the report-->
     <h1> Username: $userDetails</h1>
-    <p>Uptime: $uptime</p>
+    <p>Last Boot<br> $lastBoot</p>
     <p>Base CPU Speed: $basecpuSpeed Ghz</p>
     <p>Current CPU Speed: $trimmedActualCPUSpeed Ghz</p>
-    <p>Domain Connection status: $domainName / $domainCheck</p>
+    <p>Domain Connection Status: $domainName / $domainCheck</p>
+    <p>Battery Details</p>
+    <ul>
+        <li>Designed Capacity: $designedCapacity</li>
+        <li>Actual Capacity: $actualCapacity mWh</li>
+        <li>Battery Capacity Percentage: $capacityPercentage%</li>
+    </ul>
 </body>
 </html>
 "@
